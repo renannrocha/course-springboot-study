a91214e4ce31753fd0ed6ac09bdf994ae007b367
renannrocha <renanrocha8897@gmail.com>
2024-02-09 11:19:07 -0300

    hello word with spring boot

--- springstudypartone/src/main/java/com/springcourse/sectiontwo/springstudypartone/SpringstudypartoneApplication.java
+++ springstudypartone/src/main/java/com/springcourse/sectiontwo/springstudypartone/SpringstudypartoneApplication.java
@@ -2,9 +2,17 @@ package com.springcourse.sectiontwo.springstudypartone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringstudypartoneApplication {
+
+	@GetMapping("/hello")
+	public String helloWord(){
+		return "Hello Word !";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringstudypartoneApplication.class, args);
