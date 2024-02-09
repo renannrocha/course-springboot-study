f5750bd79ca8e6858f95dedf763c2d033facf492
renannrocha <renanrocha8897@gmail.com>
2024-02-09 11:13:51 -0300

    intial commit - sesstart sesection 02 SpringBoot core

---
+++ springstudypartone/src/main/java/com/springcourse/sectiontwo/springstudypartone/SpringstudypartoneApplication.java
@@ -0,0 +1,13 @@
+package com.springcourse.sectiontwo.springstudypartone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstudypartoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstudypartoneApplication.class, args);
+	}
+
+}
---
+++ springstudypartone/src/test/java/com/springcourse/sectiontwo/springstudypartone/SpringstudypartoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcourse.sectiontwo.springstudypartone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudypartoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
