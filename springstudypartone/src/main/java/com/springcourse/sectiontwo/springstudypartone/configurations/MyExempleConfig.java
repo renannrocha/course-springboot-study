c4bbcc01314f2e56c8ea07d410925a7cf9119eaa
renannrocha <renanrocha8897@gmail.com>
2024-02-09 22:20:37 -0300

    criando anotations customizadas de configuração - não entendi muito

--- springstudypartone/src/main/java/com/springcourse/sectiontwo/springstudypartone/configurations/MyExempleConfig.java
+++ springstudypartone/src/main/java/com/springcourse/sectiontwo/springstudypartone/configurations/MyExempleConfig.java
@@ -1,20 +1,18 @@
 package com.springcourse.sectiontwo.springstudypartone.configurations;
 
+import com.springcourse.sectiontwo.springstudypartone.Development;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development") // determina em qual perfil as configurações serão autorizadas
+@Development // criar anotation customizadas para referenciar as configurações
 public class MyExempleConfig {
 
-    // testar se aplicação esta rodando no perfil development
     @Bean
     public CommandLineRunner executar(){
         return args -> {
               System.out.println("Aplicação executando em anbiente de desenvolvimento");
         };
     }
-
 }
