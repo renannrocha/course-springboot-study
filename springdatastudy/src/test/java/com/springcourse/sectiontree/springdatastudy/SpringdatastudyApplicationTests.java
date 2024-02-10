ac8f06466e37f318dfda324f0af1a311f739f803
renannrocha <renanrocha8897@gmail.com>
2024-02-09 22:38:37 -0300

    create 2°project springdatatudy

---
+++ springdatastudy/src/test/java/com/springcourse/sectiontree/springdatastudy/SpringdatastudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcourse.sectiontree.springdatastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatastudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
