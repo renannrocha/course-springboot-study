8d69dbe8565f4c536e46122d29df2abfd4056e51
renannrocha <renanrocha8897@gmail.com>
2024-02-16 17:18:28 -0300

    mapeando as requisições com @RequestMapping

--- springApiRestfulStudy/src/main/java/com/springcourse/sectionfour/springApiRestfulStudy/rest/controller/ClienteController.java
+++ springApiRestfulStudy/src/main/java/com/springcourse/sectionfour/springApiRestfulStudy/rest/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.springcourse.sectionfour.springApiRestfulStudy.rest.controller;
 
+import com.springcourse.sectionfour.springApiRestfulStudy.domain.entites.Cliente;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nomeCliente}", method = RequestMethod.GET)
+    @RequestMapping(
+            value = "/hello/{nome}",
+            method = RequestMethod.GET,
+            // forma de como recebemos um objeto
+            consumes = {"aplication/json" , "aplication/xml"}, // faz mais sentido com metodo POST,
+            // forma de retorno dos nossos objetos
+            produces = {"aplication/json" , "aplication/xml"}
+    )
     @ResponseBody
-    public String helloClientes (@PathVariable String nomeCliente){
+    public String helloClientes (@PathVariable("nome") String nomeCliente){
         return String.format("Hello %s ", nomeCliente);
     }
 
